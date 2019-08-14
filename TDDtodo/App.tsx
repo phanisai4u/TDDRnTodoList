2cc8f5c3ce5443b3af4cd33fcd3e596897487325
phanisai <saikumar.chapparam@hexagonsi.com>
2019-08-14 10:43:28 +0530

    Some working example witl test's

--- TDDtodo/App.tsx
+++ TDDtodo/App.tsx
@@ -7,18 +7,45 @@ import {
 
 
 export const add = (a: number, b: number) => a + b;
-const App = () => {
-  
-  return (
-    <View style={styles.appContent}>
-      <Text> hi</Text>
-    </View>
-  );
+export class App extends React.Component {
+
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      test: true
+    }
+  }
+
+  internalAdd(a: number, b: number) {
+    return a + b;
+  }
+
+  componentDidMount() {
+    this.setState({ test: false })
+  }
+
+  changeTest (val:boolean) {
+    this.setState({ test: val })
+  }
+  render() {
+    const test: boolean = this.state.test || false
+    return (
+      test ? <View style={styles.appContent}>
+        <Text> Test is please case</Text>
+       </View> : <View style={styles.appContent}>
+          <Text> hi</Text>
+        </View>
+        
+    )
+
+  }
 };
+
+
 
 const styles = StyleSheet.create({
   appContent: {
-    backgroundColor:'gray',
+    backgroundColor: 'gray',
   }
 });
 
--- TDDtodo/__tests__/App-test.tsx
+++ TDDtodo/__tests__/App-test.tsx
@@ -17,6 +17,10 @@ describe('App', () => {
 
   describe('rendering', () => {
 
+    it('Shallow rendering', () => {
+      expect(wrapper).toMatchSnapshot()
+    })
+
     it('should render a <View />', () => {
       expect(wrapper.find('View')).toHaveLength(1);
     });
@@ -24,7 +28,30 @@ describe('App', () => {
     it('should add two numbers', () => {
       expect(add(1, 1)).toEqual(2);
     });
+
+    //Test class internal functions
+    it('should add two numbers', () => {
+      //NOTE:- Here wrapper must be React element other wise instance() function  will return null
+      expect(wrapper.instance().internalAdd(1, 1)).toEqual(2);
+    });
     
+    it('has state', () => {
+      expect(wrapper.state('test')).toBe(false);
+    });
+
+    // //For negative test case
+    // it('has state', () => {
+    //   expect(wrapper.state('test')).toBe(true);
+    // });
+
+    //Update here  
+    //after update this TestCase will be true
+    it ('update state',() => {
+      wrapper.instance().changeTest(true)
+      expect(wrapper.state('test')).toBe(true);
+
+    })
+
   });
 });
 
